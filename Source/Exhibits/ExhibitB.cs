ae17dfcc6aac94e3dbffdf8f6efe884af1dadbd9
cramps-man <cramps-man@hotmail.com>
2024-09-03 21:29:27 +1000

    Add exhibitB extra effects

--- Source/Exhibits/ExhibitB.cs
+++ Source/Exhibits/ExhibitB.cs
@@ -1,14 +1,17 @@
 ﻿using LBoL.Base;
 using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.StatusEffects;
 using LBoL.EntityLib.Exhibits;
 using LBoLEntitySideloader;
 using LBoLEntitySideloader.Attributes;
 using LBoLEntitySideloader.Entities;
 using LBoLEntitySideloader.Resource;
 using LBoLMod.PlayerUnits;
-using System;
+using LBoLMod.StatusEffects;
 using System.Collections.Generic;
-using System.Text;
 
 namespace LBoLMod.Exhibits
 {
@@ -46,9 +49,9 @@ namespace LBoLMod.Exhibits
                 Value1: null,
                 Value2: null,
                 Value3: null,
-                Mana: new ManaGroup() { },
+                Mana: new ManaGroup() { Green = 1 },
                 BaseManaRequirement: null,
-                BaseManaColor: ManaColor.White,
+                BaseManaColor: ManaColor.Green,
                 BaseManaAmount: 1,
                 HasCounter: false,
                 InitialCounter: null,
@@ -63,6 +66,26 @@ namespace LBoLMod.Exhibits
     [EntityLogic(typeof(ExhibitBDef))]
     public sealed class ExhibitB: ShiningExhibit
     {
+        protected override void OnEnterBattle()
+        {
+            base.ReactBattleEvent<StatusEffectApplyEventArgs>(base.Battle.Player.StatusEffectAdded, new EventSequencedReactor<StatusEffectApplyEventArgs>(this.onStatusApplied));
+        }
 
+        private IEnumerable<BattleAction> onStatusApplied(StatusEffectApplyEventArgs args)
+        {
+            if (args.Effect is PowerStance)
+            {
+                yield return new ApplyStatusEffectAction<TempFirepower>(base.Battle.Player, 1);
+            }
+            if (args.Effect is FocusStance)
+            {
+                yield return new DrawCardAction();
+            }
+            if (args.Effect is CalmStance)
+            {
+                yield return new CastBlockShieldAction(base.Battle.Player, base.Battle.Player, 4, 0);
+            }
+            yield break;
+        }
     }
 }
