9ad916f886d1e6df8129db0889a98e6ad6350be2
cramps-man <cramps-man@hotmail.com>
2024-09-09 00:56:18 +1000

    Add EnterPower card

---
+++ Source/Cards/General/EnterPower.cs
@@ -0,0 +1,47 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class EnterPowerDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(EnterPower);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Type = CardType.Attack;
+            cardConfig.TargetType = TargetType.SingleEnemy;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.Red };
+            cardConfig.Damage = 10;
+            cardConfig.UpgradedDamage = 12;
+            cardConfig.Cost = new ManaGroup() { Any = 1, Red = 1 };
+            cardConfig.UpgradedCost = new ManaGroup() { Red = 1 };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(EnterPowerDef))]
+    public sealed class EnterPower : Card
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            
+            yield return base.AttackAction(selector);
+            if (!base.Battle.BattleShouldEnd)
+            {
+                yield return StanceUtils.ApplyStance<PowerStance>(this);
+            }
+        }
+    }
+}
