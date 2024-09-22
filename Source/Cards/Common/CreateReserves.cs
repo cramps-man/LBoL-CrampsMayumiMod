d82d859852a88c68ea89b1308676ef877d6279b5
cramps-man <cramps-man@hotmail.com>
2024-09-22 20:47:49 +1000

    Update CreateReserves to exile and add createhaniwa

--- Source/Cards/Common/CreateReserves.cs
+++ Source/Cards/Common/CreateReserves.cs
@@ -3,10 +3,12 @@ using LBoL.ConfigData;
 using LBoL.Core;
 using LBoL.Core.Battle;
 using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Battle.Interactions;
 using LBoL.Core.Cards;
 using LBoLEntitySideloader;
 using LBoLEntitySideloader.Attributes;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LBoLMod.Cards
 {
@@ -21,11 +23,15 @@ namespace LBoLMod.Cards
         {
             var cardConfig = base.MakeConfig();
             cardConfig.Type = CardType.Skill;
-            cardConfig.Colors = new List<ManaColor>() { ManaColor.Red, ManaColor.White };
-            cardConfig.Cost = new ManaGroup() { Any = 1 };
-            cardConfig.UpgradedCost = new ManaGroup() { Any = 0 };
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
+            cardConfig.Cost = new ManaGroup() { White = 1 };
+            cardConfig.UpgradedCost = new ManaGroup() { Any = 1 };
+            cardConfig.Value1 = 1;
+            cardConfig.UpgradedValue1 = 2;
+            cardConfig.Keywords = Keyword.Exile;
+            cardConfig.UpgradedKeywords = Keyword.Exile;
             cardConfig.RelativeCards = new List<string>() { nameof(CreateHaniwa) };
-            cardConfig.UpgradedRelativeCards = new List<string>() { nameof(CreateHaniwa)+"+" };
+            cardConfig.UpgradedRelativeCards = new List<string>() { nameof(CreateHaniwa) };
             return cardConfig;
         }
     }
@@ -33,9 +39,22 @@ namespace LBoLMod.Cards
     [EntityLogic(typeof(CreateReservesDef))]
     public sealed class CreateReserves : Card
     {
+        public override Interaction Precondition()
+        {
+            List<Card> list = base.Battle.HandZone.Where((Card hand) => hand != this).ToList();
+            if (list.Count <= 0)
+            {
+                return null;
+            }
+            return new SelectHandInteraction(0, Value1, list);
+        }
         protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
         {
-            yield return new AddCardsToHandAction(new Card[] { Library.CreateCard<CreateHaniwa>(IsUpgraded) });
+            if (precondition == null)
+                yield break;
+            IReadOnlyList<Card> cards = ((SelectHandInteraction)precondition).SelectedCards;
+            yield return new ExileManyCardAction(cards);
+            yield return new AddCardsToHandAction(Library.CreateCards<CreateHaniwa>(cards.Count));
         }
     }
 }
