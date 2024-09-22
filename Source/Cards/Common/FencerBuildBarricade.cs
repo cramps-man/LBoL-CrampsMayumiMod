d5d836d116e00d2755d7ce2ab31f0063141abe19
cramps-man <cramps-man@hotmail.com>
2024-09-22 23:06:01 +1000

    Add card FencerBuildBarricade

---
+++ Source/Cards/Common/FencerBuildBarricade.cs
@@ -0,0 +1,52 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects;
+using LBoLMod.StatusEffects.Assign;
+using LBoLMod.StatusEffects.Keywords;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class FencerBuildBarricadeDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(FencerBuildBarricade);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Type = CardType.Defense;
+            cardConfig.TargetType = TargetType.Nobody;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
+            cardConfig.Cost = new ManaGroup() { Any = 0 };
+            cardConfig.Shield = 12;
+            cardConfig.UpgradedShield = 16;
+            cardConfig.Value2 = 9;
+            cardConfig.UpgradedValue2 = 5;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Haniwa), nameof(Assign) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Haniwa), nameof(Assign) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(FencerBuildBarricadeDef))]
+    public sealed class FencerBuildBarricade : Card
+    {
+        public override bool CanUse => HaniwaUtils.IsLevelFulfilled<FencerHaniwa>(base.Battle.Player, FencerRequired);
+        public override string CantUseMessage => "Need more Fencer";
+        public int FencerRequired => 2;
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return HaniwaUtils.SacrificeHaniwa<FencerHaniwa>(base.Battle.Player, FencerRequired);
+            yield return new ApplyStatusEffectAction<AssignFencerBuildBarricade>(base.Battle.Player, Shield.Shield, FencerRequired, Value2);
+        }
+    }
+}
---
+++ Source/StatusEffects/Assign/AssignFencerBuildBarricade.cs
@@ -0,0 +1,23 @@
+﻿using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoLEntitySideloader;
+using System.Collections.Generic;
+
+namespace LBoLMod.StatusEffects.Assign
+{
+    public sealed class AssignFencerBuildBarricadeDef : ModAssignStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(AssignFencerBuildBarricade);
+        }
+    }
+    public sealed class AssignFencerBuildBarricade : ModAssignStatusEffect
+    {
+        protected override IEnumerable<BattleAction> OnAssignmentDone()
+        {
+            yield return new CastBlockShieldAction(base.Battle.Player, 0, Level);
+            yield return HaniwaUtils.ForceGainHaniwa<FencerHaniwa>(base.Battle.Player, HaniwaAssigned);
+        }
+    }
+}
