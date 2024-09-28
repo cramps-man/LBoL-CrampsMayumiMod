dfffa06cc5ff1f583f1b264791061bda16fe6062
cramps-man <cramps-man@hotmail.com>
2024-09-28 22:33:02 +1000

    Add card DiscardFrontline

---
+++ Source/Cards/Common/Red/DiscardFrontline.cs
@@ -0,0 +1,57 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Battle.Interactions;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Keywords;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LBoLMod.Cards
+{
+    public sealed class DiscardFrontlineDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(DiscardFrontline);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Type = CardType.Skill;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.Red };
+            cardConfig.Cost = new ManaGroup() { Red = 1 };
+            cardConfig.UpgradedCost = new ManaGroup() { Red = 1 };
+            cardConfig.Value1 = 1;
+            cardConfig.UpgradedValue1 = 3;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Frontline) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Frontline) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(DiscardFrontlineDef))]
+    public sealed class DiscardFrontline : Card
+    {
+        public override Interaction Precondition()
+        {
+            List<Card> list = base.Battle.HandZone.Where((Card hand) => hand is ModFrontlineCard && hand.IsExile).ToList();
+            return new SelectHandInteraction(0, Value1, list);
+        }
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            IReadOnlyList<Card> cards = ((SelectHandInteraction)precondition).SelectedCards;
+            foreach (ModFrontlineCard card in cards)
+            {
+                card.IsExile = false;
+                card.IsReplenish = true;
+            }
+            yield return new DiscardManyAction(cards);
+        }
+    }
+}
