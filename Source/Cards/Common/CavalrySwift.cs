37459adee78d30c6eca9a15229f9ffe3fd6619ff
cramps-man <cramps-man@hotmail.com>
2024-09-29 18:13:03 +1000

    Update CavalrySwift
    - Still need to change the name probs?

--- Source/Cards/Common/CavalrySwift.cs
+++ Source/Cards/Common/CavalrySwift.cs
@@ -4,13 +4,12 @@ using LBoL.Core;
 using LBoL.Core.Battle;
 using LBoL.Core.Battle.BattleActions;
 using LBoL.Core.Cards;
-using LBoL.Core.StatusEffects;
 using LBoLEntitySideloader;
 using LBoLEntitySideloader.Attributes;
 using LBoLMod.StatusEffects;
 using LBoLMod.StatusEffects.Keywords;
-using LBoLMod.Utils;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LBoLMod.Cards
 {
@@ -26,12 +25,12 @@ namespace LBoLMod.Cards
             var cardConfig = base.MakeConfig();
             cardConfig.Type = CardType.Skill;
             cardConfig.Colors = new List<ManaColor>() { ManaColor.Red, ManaColor.White };
-            cardConfig.Cost = new ManaGroup() { Any = 1, Hybrid = 1, HybridColor = 2 };
+            cardConfig.Cost = new ManaGroup() { Hybrid = 1, HybridColor = 2 };
             cardConfig.UpgradedCost = new ManaGroup() { Any = 1 };
-            cardConfig.Value1 = 2;
-            cardConfig.Value2 = 1;
-            cardConfig.RelativeEffects = new List<string>() { nameof(Haniwa), nameof(Graze) };
-            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Haniwa), nameof(Graze) };
+            cardConfig.Value1 = 1;
+            cardConfig.UpgradedValue1 = 2;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Frontline), nameof(Assign) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Frontline), nameof(Assign) };
             return cardConfig;
         }
     }
@@ -39,14 +38,17 @@ namespace LBoLMod.Cards
     [EntityLogic(typeof(CavalrySwiftDef))]
     public sealed class CavalrySwift : Card
     {
-        public int CavalryRequired => 1;
+        public int AssignRequirement => 2;
+        public int FrontlineRequirement => 3;
         protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
         {
-            if (HaniwaUtils.IsLevelFulfilled<CavalryHaniwa>(base.Battle.Player, CavalryRequired, HaniwaActionType.Require))
-            {
-                yield return new ApplyStatusEffectAction<Graze>(base.Battle.Player, Value2);
-            }
             yield return new DrawManyCardAction(Value1);
+            var assignStatuses = base.Battle.Player.StatusEffects.Where(s => s is ModAssignStatusEffect).ToList().Count;
+            if (assignStatuses >= AssignRequirement)
+                yield return new DrawCardAction();
+            var frontlineCards = base.Battle.HandZone.Where((Card c) => c is ModFrontlineCard).ToList().Count;
+            if (frontlineCards >= FrontlineRequirement)
+                yield return new DrawCardAction();
         }
     }
 }
