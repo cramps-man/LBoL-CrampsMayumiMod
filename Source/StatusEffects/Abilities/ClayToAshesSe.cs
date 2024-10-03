09598e83e5747aea5fd49f0dcca229d3a15da0e0
cramps-man <cramps-man@hotmail.com>
2024-10-03 19:59:24 +1000

    Add card ClayToAshes

---
+++ Source/StatusEffects/Abilities/ClayToAshesSe.cs
@@ -0,0 +1,46 @@
+﻿using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.StatusEffects;
+using LBoL.Core.Units;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.BattleActions;
+using LBoLMod.Cards;
+using LBoLMod.GameEvents;
+using System.Collections.Generic;
+
+namespace LBoLMod.StatusEffects.Abilities
+{
+    public sealed class ClayToAshesSeDef : ModStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(ClayToAshesSe);
+        }
+    }
+
+    [EntityLogic(typeof(ClayToAshesSeDef))]
+    public sealed class ClayToAshesSe: StatusEffect
+    {
+        protected override void OnAdded(Unit unit)
+        {
+            base.ReactOwnerEvent(ModGameEvents.SacrificedHaniwa, this.OnHaniwaSacrificed);
+            base.ReactOwnerEvent(base.Battle.CardExiled, this.OnCardExiled);
+        }
+
+        private IEnumerable<BattleAction> OnCardExiled(CardEventArgs args)
+        {
+            if (base.Battle.BattleShouldEnd)
+                yield break;
+            if (args.Card is ModFrontlineCard)
+                yield return BuffAction<TempFirepower>(Level);
+        }
+
+        private IEnumerable<BattleAction> OnHaniwaSacrificed(LoseHaniwaEventArgs args)
+        {
+            if (base.Battle.BattleShouldEnd)
+                yield break;
+            yield return BuffAction<TempFirepower>(Level);
+        }
+    }
+}
