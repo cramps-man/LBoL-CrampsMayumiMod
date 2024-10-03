958a346317781e24e574ac9d1bf93e7c13728f25
cramps-man <cramps-man@hotmail.com>
2024-10-03 16:22:07 +1000

    Add card HaniwaBlitz

--- Source/Cards/Common/Red/HaniwaBlitz.cs
+++ Source/Cards/Common/Red/HaniwaBlitz.cs
@@ -1,74 +1,55 @@
 ﻿using LBoL.Base;
-using LBoL.Base.Extensions;
 using LBoL.ConfigData;
 using LBoL.Core;
 using LBoL.Core.Battle;
 using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Battle.Interactions;
 using LBoL.Core.Cards;
 using LBoLEntitySideloader;
 using LBoLEntitySideloader.Attributes;
-using LBoLMod.Utils;
 using LBoLMod.StatusEffects.Keywords;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using LBoL.Core.Battle.Interactions;
 
 namespace LBoLMod.Cards
 {
-    public sealed class HaniwaCommanderDef : ModCardTemplate
+    public sealed class HaniwaBlitzDef : ModCardTemplate
     {
         public override IdContainer GetId()
         {
-            return nameof(HaniwaCommander);
+            return nameof(HaniwaBlitz);
         }
 
         public override CardConfig MakeConfig()
         {
             var cardConfig = base.MakeConfig();
-            cardConfig.IsPooled = false;
-            cardConfig.Rarity = Rarity.Uncommon;
-            cardConfig.Type = CardType.Skill;
+            cardConfig.Type = CardType.Attack;
             cardConfig.TargetType = TargetType.SingleEnemy;
             cardConfig.Colors = new List<ManaColor>() { ManaColor.Red };
-            cardConfig.Value1 = 3;
-            cardConfig.UpgradedValue1 = 5;
-            cardConfig.Keywords = Keyword.Retain | Keyword.Exile;
-            cardConfig.UpgradedKeywords = Keyword.Retain | Keyword.Exile;
+            cardConfig.Cost = new ManaGroup() { Red = 1, Any = 1 };
+            cardConfig.Damage = 12;
+            cardConfig.UpgradedDamage = 15;
+            cardConfig.Value1 = 1;
+            cardConfig.UpgradedValue1 = 2;
             cardConfig.RelativeEffects = new List<string>() { nameof(Frontline) };
             cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Frontline) };
             return cardConfig;
         }
     }
 
-    [EntityLogic(typeof(HaniwaCommanderDef))]
-    public sealed class HaniwaCommander : ModFrontlineCard
+    [EntityLogic(typeof(HaniwaBlitzDef))]
+    public sealed class HaniwaBlitz : Card
     {
-        protected override void OnEnterBattle(BattleController battle)
-        {
-            base.OnEnterBattle(battle);
-            base.ReactBattleEvent(base.Battle.Player.TurnStarted, this.OnPlayerTurnStarted);
-        }
-
-        private IEnumerable<BattleAction> OnPlayerTurnStarted(UnitEventArgs args)
-        {
-            if (base.Battle.BattleShouldEnd)
-                yield break;
-            if (base.Zone != CardZone.Hand)
-                yield break;
-
-            base.NotifyActivating();
-            Type commonType = HaniwaFrontlineUtils.CommonSummonTypes.SampleOrDefault(base.BattleRng);
-            yield return new AddCardsToHandAction(Library.CreateCard(commonType));
-        }
         public override Interaction Precondition()
         {
-            List<Card> list = base.Battle.HandZone.Where(c => c != this && c is ModFrontlineCard && !(c is HaniwaCommander)).ToList();
+            List<Card> list = base.Battle.HandZone.Where((Card c) => c != this && c is ModFrontlineCard && !(c is HaniwaCommander)).ToList();
             return new SelectHandInteraction(0, Value1, list);
         }
-
         protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
         {
+            yield return AttackAction(selector);
+            if (base.Battle.BattleShouldEnd)
+                yield break;
             if (!(precondition is SelectHandInteraction selectInteraction))
                 yield break;
 
