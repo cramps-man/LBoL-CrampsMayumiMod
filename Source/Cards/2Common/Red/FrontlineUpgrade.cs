582892b820ac8d16e2a01e65902e65154ff2fa96
cramps-man <cramps-man@hotmail.com>
2024-10-06 22:16:59 +1100

    Add card FrontlineUpgrade

---
+++ Source/Cards/2Common/Red/FrontlineUpgrade.cs
@@ -0,0 +1,65 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Battle.Interactions;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Keywords;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LBoLMod.Cards
+{
+    public sealed class FrontlineUpgradeDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(FrontlineUpgrade);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Type = CardType.Skill;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.Red };
+            cardConfig.Cost = new ManaGroup() { Red = 1 };
+            cardConfig.Block = 6;
+            cardConfig.UpgradedBlock = 8;
+            cardConfig.Value1 = 1;
+            cardConfig.Value2 = 1;
+            cardConfig.UpgradedValue2 = 2;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Frontline) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Frontline) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(FrontlineUpgradeDef))]
+    public sealed class FrontlineUpgrade : Card
+    {
+        public override Interaction Precondition()
+        {
+            List<Card> list = base.Battle.HandZone.Where((Card c) => c != this && (c is ModFrontlineCard || !c.IsUpgraded)).ToList();
+            return new SelectHandInteraction(0, Value1, list);
+        }
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return DefenseAction();
+            if (!(precondition is SelectHandInteraction selectInteraction))
+                yield break;
+
+            yield return new UpgradeCardsAction(selectInteraction.SelectedCards);
+            foreach (var card in selectInteraction.SelectedCards)
+            {
+                if (card is ModFrontlineCard)
+                {
+                    for (int i = 0; i < Value2; i++)
+                        yield return new UpgradeCardAction(card);
+                }
+            }
+        }
+    }
+}
