394d2e034e1e858d939fd54e521007eba090b457
cramps-man <cramps-man@hotmail.com>
2024-10-06 22:58:06 +1100

    Add card AssignFastTrigger

---
+++ Source/Cards/2Common/Yellow/AssignFastTrigger.cs
@@ -0,0 +1,57 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.Interactions;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Keywords;
+using LBoLMod.Utils;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class AssignFastTriggerDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(AssignFastTrigger);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Type = CardType.Attack;
+            cardConfig.TargetType = TargetType.SingleEnemy;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
+            cardConfig.Damage = 13;
+            cardConfig.UpgradedDamage = 17;
+            cardConfig.Cost = new ManaGroup() { White = 2 };
+            cardConfig.UpgradedCost = new ManaGroup() { White = 1, Any = 1 };
+            cardConfig.RelativeEffects = new List<string>() { nameof(Assign) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Assign) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(AssignFastTriggerDef))]
+    public sealed class AssignFastTrigger : Card
+    {
+        public override Interaction Precondition()
+        {
+            return new SelectCardInteraction(0, 1, HaniwaAssignUtils.CreateAssignOptionCards(base.Battle.Player));
+        }
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return AttackAction(selector);
+            if (base.Battle.BattleShouldEnd)
+                yield break;
+
+            foreach (ModAssignOptionCard card in ((SelectCardInteraction)precondition).SelectedCards)
+            {
+                card.StatusEffect.TickdownFull();
+            }
+        }
+    }
+}
