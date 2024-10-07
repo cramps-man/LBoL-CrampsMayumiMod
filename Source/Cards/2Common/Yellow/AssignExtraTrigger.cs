21bae0c9478f3f6b002ce6a86ac4cbf4df11dee1
cramps-man <cramps-man@hotmail.com>
2024-10-07 19:17:40 +1100

    Make AssignExtraTrigger common and give block

--- Source/Cards/2Common/Yellow/AssignExtraTrigger.cs
+++ Source/Cards/2Common/Yellow/AssignExtraTrigger.cs
@@ -2,7 +2,6 @@
 using LBoL.ConfigData;
 using LBoL.Core;
 using LBoL.Core.Battle;
-using LBoL.Core.Battle.BattleActions;
 using LBoL.Core.Battle.Interactions;
 using LBoL.Core.Cards;
 using LBoLEntitySideloader;
@@ -13,21 +12,22 @@ using System.Collections.Generic;
 
 namespace LBoLMod.Cards
 {
-    public sealed class AssignExtraTimeDef : ModCardTemplate
+    public sealed class AssignExtraTriggerDef : ModCardTemplate
     {
         public override IdContainer GetId()
         {
-            return nameof(AssignExtraTime);
+            return nameof(AssignExtraTrigger);
         }
 
         public override CardConfig MakeConfig()
         {
             var cardConfig = base.MakeConfig();
-            cardConfig.Rarity = Rarity.Uncommon;
-            cardConfig.Type = CardType.Skill;
+            cardConfig.Type = CardType.Defense;
             cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
+            cardConfig.Block = 12;
+            cardConfig.UpgradedBlock = 16;
             cardConfig.Cost = new ManaGroup() { White = 2 };
-            cardConfig.UpgradedCost = new ManaGroup() { White = 1 };
+            cardConfig.UpgradedCost = new ManaGroup() { White = 1, Any = 1 };
             cardConfig.Value1 = 1;
             cardConfig.RelativeEffects = new List<string>() { nameof(Assign) };
             cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Assign) };
@@ -35,8 +35,8 @@ namespace LBoLMod.Cards
         }
     }
 
-    [EntityLogic(typeof(AssignExtraTimeDef))]
-    public sealed class AssignExtraTime : Card
+    [EntityLogic(typeof(AssignExtraTriggerDef))]
+    public sealed class AssignExtraTrigger : Card
     {
         public override Interaction Precondition()
         {
@@ -44,11 +44,11 @@ namespace LBoLMod.Cards
         }
         protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
         {
+            yield return DefenseAction();
             foreach (ModAssignOptionCard card in ((SelectCardInteraction)precondition).SelectedCards)
             {
                 card.StatusEffect.Level++;
             }
-            yield return new DrawCardAction();
         }
     }
 }
