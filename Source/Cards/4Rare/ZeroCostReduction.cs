90b3d416a71964abc5eddb931da9a2941da22e79
cramps-man <cramps-man@hotmail.com>
2024-10-07 21:11:51 +1100

    Add card ZeroCostReduction

---
+++ Source/Cards/4Rare/ZeroCostReduction.cs
@@ -0,0 +1,40 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Abilities;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class ZeroCostReductionDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(ZeroCostReduction);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Rare;
+            cardConfig.Type = CardType.Ability;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.Red, ManaColor.White };
+            cardConfig.Cost = new ManaGroup() { White = 2, Red = 2, Any = 1 };
+            cardConfig.UpgradedCost = new ManaGroup() { White = 1, Red = 1, Any = 1 };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(ZeroCostReductionDef))]
+    public sealed class ZeroCostReduction : Card
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return BuffAction<ZeroCostReductionSe>();
+        }
+    }
+}
---
+++ Source/StatusEffects/Abilities/ZeroCostReductionSe.cs
@@ -0,0 +1,52 @@
+﻿using LBoL.Base;
+using LBoL.Base.Extensions;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Cards;
+using LBoL.Core.StatusEffects;
+using LBoL.Core.Units;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using System.Linq;
+
+namespace LBoLMod.StatusEffects.Abilities
+{
+    public sealed class ZeroCostReductionSeDef : ModStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(ZeroCostReductionSe);
+        }
+
+        public override StatusEffectConfig MakeConfig()
+        {
+            var config = base.MakeConfig();
+            config.HasLevel = false;
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(ZeroCostReductionSeDef))]
+    public sealed class ZeroCostReductionSe: StatusEffect
+    {
+        protected override void OnAdded(Unit unit)
+        {
+            base.HandleOwnerEvent(base.Battle.CardUsed, this.OnCardUsed);
+        }
+
+        private void OnCardUsed(CardUsingEventArgs args)
+        {
+            if (base.Battle.BattleShouldEnd)
+                return;
+
+            if (args.Card.Cost.IsEmpty)
+            {
+                Card card = base.Battle.HandZone.Where(c => c.Cost.Any > 0).SampleOrDefault(base.GameRun.BattleRng);
+                if (card != null)
+                {
+                    card.DecreaseTurnCost(ManaGroup.Anys(1));
+                }
+            }
+        }
+    }
+}
