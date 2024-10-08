2b23612e41c15c91b8c9465a4b2d27705918aeb0
cramps-man <cramps-man@hotmail.com>
2024-10-08 20:52:40 +1100

    Add card MassExtraTrigger

---
+++ Source/Cards/4Rare/Yellow/MassExtraTrigger.cs
@@ -0,0 +1,62 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Battle.Interactions;
+using LBoL.Core.Cards;
+using LBoL.Core.StatusEffects;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects;
+using LBoLMod.StatusEffects.Keywords;
+using LBoLMod.Utils;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LBoLMod.Cards
+{
+    public sealed class MassExtraTriggerDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(MassExtraTrigger);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Rare;
+            cardConfig.Type = CardType.Skill;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
+            cardConfig.Cost = new ManaGroup() { White = 2, Any = 1 };
+            cardConfig.UpgradedCost = new ManaGroup() { White = 1 };
+            cardConfig.Keywords = Keyword.Exile;
+            cardConfig.UpgradedKeywords = Keyword.Exile;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Assign) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Assign) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(MassExtraTriggerDef))]
+    public sealed class MassExtraTrigger : Card
+    {
+        public override Interaction Precondition()
+        {
+            return new SelectCardInteraction(0, 1, HaniwaAssignUtils.CreateAssignOptionCards(base.Battle.Player));
+        }
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            foreach (ModAssignOptionCard card in ((SelectCardInteraction)precondition).SelectedCards)
+            {
+                List<StatusEffect> removedAssignBuffs = base.Battle.Player.StatusEffects.Where(s => s is ModAssignStatusEffect && s != card.StatusEffect).ToList();
+                foreach (StatusEffect statusEffect in removedAssignBuffs)
+                {
+                    card.StatusEffect.Level += statusEffect.Level;
+                    yield return new RemoveStatusEffectAction(statusEffect);
+                }
+            }
+        }
+    }
+}
