faa93a951dd9e2b7593da3c0d5c412efb6a9ef5b
cramps-man <cramps-man@hotmail.com>
2024-10-10 19:38:00 +1100

    Add card AssignTriggerSummon

---
+++ Source/Cards/4Rare/AssignTriggerSummon.cs
@@ -0,0 +1,44 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Abilities;
+using LBoLMod.StatusEffects.Keywords;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class AssignTriggerSummonDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(AssignTriggerSummon);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Rare;
+            cardConfig.Type = CardType.Ability;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.Red, ManaColor.White };
+            cardConfig.Value1 = 1;
+            cardConfig.Cost = new ManaGroup() { White = 2, Red = 2, Any = 1 };
+            cardConfig.UpgradedCost = new ManaGroup() { White = 1, Red = 1, Any = 1 };
+            cardConfig.RelativeEffects = new List<string>() { nameof(Assign), nameof(Frontline) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Assign), nameof(Frontline) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(AssignTriggerSummonDef))]
+    public sealed class AssignTriggerSummon : Card
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return BuffAction<AssignTriggerSummonSe>(Value1);
+        }
+    }
+}
---
+++ Source/StatusEffects/Abilities/AssignTriggerSummonSe.cs
@@ -0,0 +1,54 @@
+﻿using LBoL.Base.Extensions;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Cards;
+using LBoL.Core.StatusEffects;
+using LBoL.Core.Units;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.BattleActions;
+using LBoLMod.GameEvents;
+using LBoLMod.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LBoLMod.StatusEffects.Abilities
+{
+    public sealed class AssignTriggerSummonSeDef : ModStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(AssignTriggerSummonSe);
+        }
+    }
+
+    [EntityLogic(typeof(AssignTriggerSummonSeDef))]
+    public sealed class AssignTriggerSummonSe: StatusEffect
+    {
+        protected override void OnAdded(Unit unit)
+        {
+            base.ReactOwnerEvent(ModGameEvents.AssignEffectTriggered, this.OnAssignTriggered);
+        }
+
+        private IEnumerable<BattleAction> OnAssignTriggered(AssignTriggerEventArgs args)
+        {
+            if (base.Battle.BattleShouldEnd)
+                yield break;
+
+            IEnumerable<Type> possibleSummons = HaniwaFrontlineUtils.CommonSummonTypes.Concat(HaniwaFrontlineUtils.UncommonSummonTypes);
+            List<Card> cards = new List<Card>();
+            for (int i = 0; i < Level; i++)
+            {
+                Card randomCard = Library.CreateCard(possibleSummons.Sample(base.GameRun.BattleRng));
+                cards.Add(randomCard);
+            }
+            if (cards.Count > 0)
+            {
+                base.NotifyActivating();
+                yield return new AddCardsToHandAction(cards);
+            }
+        }
+    }
+}
