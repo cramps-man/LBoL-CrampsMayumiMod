a7d10b0c67fceed25e6e6560488b4be1debd1ba6
cramps-man <cramps-man@hotmail.com>
2024-10-10 22:01:23 +1100

    Add card CavalryScout

---
+++ Source/Cards/3Uncommon/Yellow/CavalryScout.cs
@@ -0,0 +1,42 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Assign;
+using LBoLMod.StatusEffects.Keywords;
+using System;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class CavalryScoutDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(CavalryScout);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Uncommon;
+            cardConfig.Type = CardType.Skill;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
+            cardConfig.Value1 = 1;
+            cardConfig.Value2 = 3;
+            cardConfig.UpgradedValue2 = 1;
+            cardConfig.Mana = new ManaGroup() { White = 1 };
+            cardConfig.RelativeEffects = new List<string>() { nameof(Haniwa), nameof(Assign) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Haniwa), nameof(Assign) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(CavalryScoutDef))]
+    public sealed class CavalryScout : ModAssignCard
+    {
+        public override int CavalryAssigned => 1;
+        public override int StartingCardCounter => Value2;
+        public override Type AssignStatusType => typeof(AssignCavalryScout);
+    }
+}
---
+++ Source/StatusEffects/Assign/AssignCavalryScout.cs
@@ -0,0 +1,25 @@
+﻿using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using System.Collections.Generic;
+
+namespace LBoLMod.StatusEffects.Assign
+{
+    public sealed class AssignCavalryScoutDef : ModAssignStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(AssignCavalryScout);
+        }
+    }
+
+    [EntityLogic(typeof(AssignCavalryScoutDef))]
+    public sealed class AssignCavalryScout : ModAssignStatusEffect
+    {
+        protected override IEnumerable<BattleAction> OnAssignmentDone(bool onTurnStart)
+        {
+            yield return new DrawManyCardAction(CardValue1);
+        }
+    }
+}
