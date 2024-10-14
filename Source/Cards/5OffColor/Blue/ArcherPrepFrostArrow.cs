2154f7e6597e0d3ae5c459ee1c60cbae1832717e
cramps-man <cramps-man@hotmail.com>
2024-10-14 21:54:18 +1100

    Add card ArcherPrepFrostArrow

---
+++ Source/Cards/5OffColor/Blue/ArcherPrepFrostArrow.cs
@@ -0,0 +1,45 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Assign;
+using LBoLMod.StatusEffects.Keywords;
+using System;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class ArcherPrepFrostArrowDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(ArcherPrepFrostArrow);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Uncommon;
+            cardConfig.Type = CardType.Attack;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.Blue };
+            cardConfig.Cost = new ManaGroup() { Any = 0 };
+            cardConfig.Damage = 15;
+            cardConfig.UpgradedDamage = 20;
+            cardConfig.Block = 8;
+            cardConfig.UpgradedBlock = 12;
+            cardConfig.Value2 = 6;
+            cardConfig.UpgradedValue2 = 4;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Haniwa), nameof(Assign) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Haniwa), nameof(Assign) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(ArcherPrepFrostArrowDef))]
+    public sealed class ArcherPrepFrostArrow : ModAssignCard
+    {
+        public override int ArcherAssigned => 1;
+        public override int StartingCardCounter => Value2;
+        public override Type AssignStatusType => typeof(AssignArcherPrepFrostArrow);
+    }
+}
---
+++ Source/StatusEffects/Assign/AssignArcherPrepFrostArrow.cs
@@ -0,0 +1,47 @@
+﻿using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Units;
+using LBoL.EntityLib.StatusEffects.Cirno;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using System.Collections.Generic;
+
+namespace LBoLMod.StatusEffects.Assign
+{
+    public sealed class AssignArcherPrepFrostArrowDef : ModAssignStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(AssignArcherPrepFrostArrow);
+        }
+    }
+
+    [EntityLogic(typeof(AssignArcherPrepFrostArrowDef))]
+    public sealed class AssignArcherPrepFrostArrow : ModAssignStatusEffect
+    {
+        protected override void OnAdded(Unit unit)
+        {
+            base.OnAdded(unit);
+            base.ReactOwnerEvent(base.Battle.Player.DamageDealt, this.OnDamageDealt);
+        }
+
+        private IEnumerable<BattleAction> OnDamageDealt(DamageEventArgs args)
+        {
+            if (args.ActionSource != this)
+                yield break;
+            if (!args.Target.HasStatusEffect<Cold>())
+                yield break;
+
+            var player = base.Battle.Player;
+            yield return new CastBlockShieldAction(player, player, AssignSourceCard.Block);
+        }
+
+        protected override IEnumerable<BattleAction> OnAssignmentDone(bool onTurnStart)
+        {
+            var target = base.Battle.RandomAliveEnemy;
+            yield return new DamageAction(Owner, target, AssignSourceCard.Damage);
+            yield return DebuffAction<Cold>(target);
+        }
+    }
+}
