02b17a8a4e2074034f647d2dd40f68b5f2874e37
cramps-man <cramps-man@hotmail.com>
2024-10-17 20:17:36 +1100

    Buff ExileFrontlineExtraTrigger

--- Source/Cards/3Uncommon/ExileFrontlineExtraTrigger.cs
+++ Source/Cards/3Uncommon/ExileFrontlineExtraTrigger.cs
@@ -27,9 +27,9 @@ namespace LBoLMod.Cards
             cardConfig.Rarity = Rarity.Uncommon;
             cardConfig.Type = CardType.Skill;
             cardConfig.Colors = new List<ManaColor>() { ManaColor.Red, ManaColor.White };
-            cardConfig.Value1 = 1;
-            cardConfig.UpgradedValue1 = 2;
+            cardConfig.Value1 = 2;
             cardConfig.Value2 = 1;
+            cardConfig.UpgradedValue2 = 2;
             cardConfig.Cost = new ManaGroup() { Hybrid = 1, Any = 1, HybridColor = 2 };
             cardConfig.RelativeEffects = new List<string>() { nameof(Frontline), nameof(Assign) };
             cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Frontline), nameof(Assign) };
