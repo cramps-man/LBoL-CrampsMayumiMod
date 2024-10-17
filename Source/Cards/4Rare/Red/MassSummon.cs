8d1797f06f8e04e87eb28a79cf048889bc4fe679
cramps-man <cramps-man@hotmail.com>
2024-10-17 19:11:24 +1100

    Buff MassSummon

--- Source/Cards/4Rare/Red/MassSummon.cs
+++ Source/Cards/4Rare/Red/MassSummon.cs
@@ -28,8 +28,11 @@ namespace LBoLMod.Cards
             cardConfig.Rarity = Rarity.Rare;
             cardConfig.Type = CardType.Skill;
             cardConfig.Colors = new List<ManaColor>() { ManaColor.Red };
-            cardConfig.Cost = new ManaGroup() { Red = 2, Any = 1 };
+            cardConfig.Cost = new ManaGroup() { Red = 3 };
+            cardConfig.Value1 = 2;
+            cardConfig.UpgradedValue1 = 5;
             cardConfig.Keywords = Keyword.Exile;
+            cardConfig.UpgradedKeywords = Keyword.Exile;
             cardConfig.RelativeEffects = new List<string>() { nameof(Frontline) };
             cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Frontline) };
             return cardConfig;
@@ -43,13 +46,21 @@ namespace LBoLMod.Cards
         {
             IEnumerable<Type> possibleSummons = HaniwaFrontlineUtils.CommonSummonTypes.Concat(HaniwaFrontlineUtils.UncommonSummonTypes);
             int emptySlots = base.Battle.MaxHand - base.Battle.HandZone.Count;
+
+            yield return new AddCardsToHandAction(GetRandomCards(emptySlots, possibleSummons));
+            yield return new AddCardsToDrawZoneAction(GetRandomCards(Value1, possibleSummons), DrawZoneTarget.Random);
+            yield return new AddCardsToDiscardAction(GetRandomCards(Value1, possibleSummons));
+        }
+
+        private List<Card> GetRandomCards(int count, IEnumerable<Type> possibleSummons)
+        {
             List<Card> cards = new List<Card>();
-            for (int i = 0; i < emptySlots; i++) 
+            for (int i = 0; i < count; i++)
             {
                 Card randomCard = Library.CreateCard(possibleSummons.Sample(base.BattleRng));
                 cards.Add(randomCard);
             }
-            yield return new AddCardsToHandAction(cards);
+            return cards;
         }
     }
 }
