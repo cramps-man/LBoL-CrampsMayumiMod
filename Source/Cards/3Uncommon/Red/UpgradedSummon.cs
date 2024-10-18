13ed6602e56dc378afb04397667bca07f175554e
cramps-man <cramps-man@hotmail.com>
2024-10-18 19:34:42 +1100

    Rework SummonHaniwaUncommon
    - It now summons 1 frontline card that starts out upgraded

--- Source/Cards/3Uncommon/Red/UpgradedSummon.cs
+++ Source/Cards/3Uncommon/Red/UpgradedSummon.cs
@@ -8,17 +8,17 @@ using LBoL.Core.Cards;
 using LBoLEntitySideloader;
 using LBoLEntitySideloader.Attributes;
 using LBoLMod.BattleActions;
-using LBoLMod.Utils;
 using LBoLMod.StatusEffects.Keywords;
+using LBoLMod.Utils;
 using System.Collections.Generic;
 
 namespace LBoLMod.Cards
 {
-    public sealed class SummonHaniwaUncommonDef : ModCardTemplate
+    public sealed class UpgradedSummonDef : ModCardTemplate
     {
         public override IdContainer GetId()
         {
-            return nameof(SummonHaniwaUncommon);
+            return nameof(UpgradedSummon);
         }
 
         public override CardConfig MakeConfig()
@@ -28,8 +28,9 @@ namespace LBoLMod.Cards
             cardConfig.Type = CardType.Skill;
             cardConfig.Colors = new List<ManaColor>() { ManaColor.Red };
             cardConfig.Cost = new ManaGroup() { Red = 2 };
-            cardConfig.UpgradedCost = new ManaGroup() { Red = 1 };
-            cardConfig.Value1 = 1;
+            cardConfig.UpgradedCost = new ManaGroup() { Red = 1, Any = 1 };
+            cardConfig.Value1 = 3;
+            cardConfig.UpgradedValue1 = 7;
             cardConfig.RelativeEffects = new List<string>() { nameof(Frontline) };
             cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Frontline) };
             cardConfig.RelativeCards = new List<string>() { nameof(HaniwaAttacker), nameof(HaniwaBodyguard), nameof(HaniwaSharpshooter), nameof(HaniwaSupport) };
@@ -38,25 +39,27 @@ namespace LBoLMod.Cards
         }
     }
 
-    [EntityLogic(typeof(SummonHaniwaUncommonDef))]
-    public sealed class SummonHaniwaUncommon : Card
+    [EntityLogic(typeof(UpgradedSummonDef))]
+    public sealed class UpgradedSummon : Card
     {
         public override Interaction Precondition()
         {
-            List<Card> cards = HaniwaFrontlineUtils.GetUncommonCards(base.Battle, checkSacrificeRequirement: true);
-            return new MiniSelectCardInteraction(cards);
+            List<Card> cards = HaniwaFrontlineUtils.GetAllCards(base.Battle, checkSacrificeRequirement: true);
+            return new SelectCardInteraction(1, 1, cards);
         }
         protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
         {
-            if (!(precondition is MiniSelectCardInteraction interaction))
+            if (!(precondition is SelectCardInteraction interaction))
                 yield break;
 
-            ModFrontlineOptionCard optionCard = interaction.SelectedCard as ModFrontlineOptionCard;
-            if (optionCard == null)
-                yield break;
+            foreach (ModFrontlineOptionCard optionCard in interaction.SelectedCards)
+            {
+                List<Card> cards = optionCard.GetCardsToSpawn();
+                cards.ForEach(c => c.UpgradeCounter = Value1);
 
-            yield return new LoseHaniwaAction(HaniwaActionType.Sacrifice, optionCard.SelectRequireFencer, optionCard.SelectRequireArcher, optionCard.SelectRequireCavalry);
-            yield return new AddCardsToHandAction(optionCard.GetCardsToSpawn());
+                yield return new LoseHaniwaAction(HaniwaActionType.Sacrifice, optionCard.SelectRequireFencer, optionCard.SelectRequireArcher, optionCard.SelectRequireCavalry);
+                yield return new AddCardsToHandAction(cards);
+            }
         }
     }
 }
