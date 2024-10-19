51848bba0d87f449b73a0a2ce985f2b5a8f9e71b
cramps-man <cramps-man@hotmail.com>
2024-10-19 23:10:03 +1100

    Add card ExileFrontlineBlitz

---
+++ Source/StatusEffects/Abilities/ExileFrontlineBlitzSe.cs
@@ -0,0 +1,56 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.StatusEffects;
+using LBoL.Core.Units;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.Cards;
+using System.Collections.Generic;
+
+namespace LBoLMod.StatusEffects.Abilities
+{
+    public sealed class ExileFrontlineBlitzSeDef : ModStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(ExileFrontlineBlitzSe);
+        }
+
+        public override StatusEffectConfig MakeConfig()
+        {
+            var config = base.MakeConfig();
+            config.HasLevel = false;
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(ExileFrontlineBlitzSeDef))]
+    public sealed class ExileFrontlineBlitzSe: StatusEffect
+    {
+        protected override void OnAdded(Unit unit)
+        {
+            base.ReactOwnerEvent(base.Battle.CardExiled, this.OnCardExiled);
+        }
+
+        private IEnumerable<BattleAction> OnCardExiled(CardEventArgs args)
+        {
+            if (base.Battle.BattleShouldEnd)
+                yield break;
+
+            if (args.Card is ModFrontlineCard frontlineCard && !(args.Card is HaniwaCommander))
+            {
+                base.NotifyActivating();
+                frontlineCard.NotifyActivating();
+                foreach (var action in frontlineCard.GetActions(new UnitSelector(base.Battle.RandomAliveEnemy), new ManaGroup(), null, new List<DamageAction>(), false))
+                {
+                    if (base.Battle.BattleShouldEnd)
+                        yield break;
+                    yield return action;
+                }
+            }
+        }
+    }
+}
