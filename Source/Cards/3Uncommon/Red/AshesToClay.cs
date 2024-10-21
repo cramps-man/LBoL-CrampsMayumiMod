9804b94e97b4c64119f79ebe93c0c71330ee4f5c
cramps-man <cramps-man@hotmail.com>
2024-10-21 20:38:57 +1100

    Make AshesToClay cost 0

--- Source/Cards/3Uncommon/Red/AshesToClay.cs
+++ Source/Cards/3Uncommon/Red/AshesToClay.cs
@@ -27,7 +27,7 @@ namespace LBoLMod.Cards
             cardConfig.Type = CardType.Skill;
             cardConfig.TargetType = TargetType.SingleEnemy;
             cardConfig.Colors = new List<ManaColor>() { ManaColor.Red };
-            cardConfig.Cost = new ManaGroup() { Red = 1, Any = 1 };
+            cardConfig.Cost = new ManaGroup() { Any = 0 };
             cardConfig.Value1 = 2;
             cardConfig.UpgradedValue1 = 3;
             cardConfig.Value2 = 1;
