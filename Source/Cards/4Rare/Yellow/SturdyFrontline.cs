c2ece06c09c189e5ead08bf53d4d0b8322fe7be2
cramps-man <cramps-man@hotmail.com>
2024-11-01 19:18:36 +1100

    Add card SturdyFrontline

---
+++ Source/Cards/4Rare/Yellow/SturdyFrontline.cs
@@ -0,0 +1,58 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.Interactions;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Keywords;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LBoLMod.Cards
+{
+    public sealed class SturdyFrontlineDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(SturdyFrontline);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Rare;
+            cardConfig.Type = CardType.Skill;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
+            cardConfig.Cost = new ManaGroup() { White = 2 };
+            cardConfig.Value1 = 1;
+            cardConfig.UpgradedValue1 = 2;
+            cardConfig.Keywords = Keyword.Exile;
+            cardConfig.UpgradedKeywords = Keyword.Exile;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Frontline) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Frontline) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(SturdyFrontlineDef))]
+    public sealed class SturdyFrontline : Card
+    {
+        public override Interaction Precondition()
+        {
+            List<Card> list = base.Battle.HandZone.Where((Card c) => c is ModFrontlineCard).ToList();
+            return new SelectHandInteraction(1, Value1, list);
+        }
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            if (!(precondition is SelectHandInteraction removeExileInteraction))
+                yield break;
+
+            foreach (var card in removeExileInteraction.SelectedCards)
+            {
+                card.IsExile = false;
+            }
+        }
+    }
+}
