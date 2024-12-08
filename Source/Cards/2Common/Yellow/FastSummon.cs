f0233960a992f552f6560997f15de2f582130adb
cramps-man <cramps-man@hotmail.com>
2024-12-08 18:41:37 +1100

    Add card FastSummon

---
+++ Source/Cards/2Common/Yellow/FastSummon.cs
@@ -0,0 +1,56 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.Interactions;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.Utils;
+using LBoLMod.StatusEffects.Keywords;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class FastSummonDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(FastSummon);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Type = CardType.Skill;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
+            cardConfig.Cost = new ManaGroup() { White = 1 };
+            cardConfig.UpgradedCost = new ManaGroup() { Any = 0 };
+            cardConfig.Value1 = 1;
+            cardConfig.Keywords = Keyword.Initial | Keyword.Exile;
+            cardConfig.UpgradedKeywords = Keyword.Initial | Keyword.Exile | Keyword.Replenish;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Frontline), nameof(Sacrifice), nameof(Haniwa) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Frontline), nameof(Sacrifice), nameof(Haniwa) };
+            cardConfig.RelativeCards = new List<string>() { nameof(HaniwaAttacker), nameof(HaniwaBodyguard), nameof(HaniwaSharpshooter), nameof(HaniwaSupport) };
+            cardConfig.UpgradedRelativeCards = new List<string>() { nameof(HaniwaAttacker), nameof(HaniwaBodyguard), nameof(HaniwaSharpshooter), nameof(HaniwaSupport) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(FastSummonDef))]
+    public sealed class FastSummon : Card
+    {
+        public override Interaction Precondition()
+        {
+            List<Card> cards = HaniwaFrontlineUtils.GetAllCards(base.Battle, checkSacrificeRequirement: true);
+            return new SelectCardInteraction(1, Value1, cards);
+        }
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            foreach (var action in HaniwaFrontlineUtils.CardsSummon(((SelectCardInteraction)precondition).SelectedCards))
+            {
+                yield return action;
+            };
+        }
+    }
+}
