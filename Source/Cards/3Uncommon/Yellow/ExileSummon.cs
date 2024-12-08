27544b3458e947f9404f9ac7e51221154b0e8571
cramps-man <cramps-man@hotmail.com>
2024-12-08 18:54:09 +1100

    ExileSummon now uncommon with slight change

--- Source/Cards/3Uncommon/Yellow/ExileSummon.cs
+++ Source/Cards/3Uncommon/Yellow/ExileSummon.cs
@@ -24,13 +24,14 @@ namespace LBoLMod.Cards
         public override CardConfig MakeConfig()
         {
             var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Uncommon;
             cardConfig.Type = CardType.Skill;
             cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
             cardConfig.Cost = new ManaGroup() { White = 1 };
             cardConfig.UpgradedCost = new ManaGroup() { Any = 1 };
             cardConfig.Value1 = 1;
-            cardConfig.UpgradedValue1 = 2;
             cardConfig.Value2 = 1;
+            cardConfig.UpgradedValue2 = 2;
             cardConfig.Keywords = Keyword.Exile;
             cardConfig.UpgradedKeywords = Keyword.Exile;
             cardConfig.RelativeEffects = new List<string>() { nameof(Frontline) };
@@ -47,14 +48,14 @@ namespace LBoLMod.Cards
         public override Interaction Precondition()
         {
             List<Card> list = base.Battle.HandZone.Where((Card hand) => hand != this).ToList();
-            return new SelectHandInteraction(Value2, Value2, list);
+            return new SelectHandInteraction(0, Value2, list);
         }
         protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
         {
             if (!(precondition is SelectHandInteraction exileInteraction))
                 yield break;
 
-            var summonInteraction = new SelectCardInteraction(0, Value1, HaniwaFrontlineUtils.AllSummonTypes.ConvertAll(t => Library.CreateCard(t)));
+            var summonInteraction = new SelectCardInteraction(0, Value1 + exileInteraction.SelectedCards.Count, HaniwaFrontlineUtils.AllSummonTypes.ConvertAll(t => Library.CreateCard(t)));
             yield return new InteractionAction(summonInteraction);
 
             yield return new ExileManyCardAction(exileInteraction.SelectedCards);
