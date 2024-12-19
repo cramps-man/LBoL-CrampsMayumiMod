5e09763c27e3d03a65a9dd5dfd0d103e9ce5bfb2
cramps-man <cramps-man@hotmail.com>
2024-12-20 00:25:24 +1100

    Add card AssignSeparation

---
+++ Source/Cards/5OffColor/Purple/AssignSeparation.cs
@@ -0,0 +1,42 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Abilities;
+using LBoLMod.StatusEffects.Keywords;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class AssignSeparationDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(AssignSeparation);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Uncommon;
+            cardConfig.Type = CardType.Ability;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.Black, ManaColor.Red };
+            cardConfig.Cost = new ManaGroup() { Hybrid = 1, Any = 1, HybridColor = 7 };
+            cardConfig.RelativeEffects = new List<string>() { nameof(Assign) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Assign) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(AssignSeparationDef))]
+    public sealed class AssignSeparation : Card
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return BuffAction<AssignSeparationSe>();
+        }
+    }
+}
---
+++ Source/StatusEffects/Abilities/AssignSeparationSe.cs
@@ -0,0 +1,27 @@
+﻿using LBoL.ConfigData;
+using LBoL.Core.StatusEffects;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+
+namespace LBoLMod.StatusEffects.Abilities
+{
+    public sealed class AssignSeparationSeDef : ModStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(AssignSeparationSe);
+        }
+
+        public override StatusEffectConfig MakeConfig()
+        {
+            var config = base.MakeConfig();
+            config.HasLevel = false;
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(AssignSeparationSeDef))]
+    public sealed class AssignSeparationSe: StatusEffect
+    {
+    }
+}
