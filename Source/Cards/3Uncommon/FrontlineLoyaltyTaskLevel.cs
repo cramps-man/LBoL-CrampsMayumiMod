135f4db8f98b98fb28eccf01c3fb5202f0442e67
cramps-man <cramps-man@hotmail.com>
2024-12-23 19:40:04 +1100

    FrontlineLoyaltyTaskLevel rework into ability

--- Source/Cards/3Uncommon/FrontlineLoyaltyTaskLevel.cs
+++ Source/Cards/3Uncommon/FrontlineLoyaltyTaskLevel.cs
@@ -2,15 +2,12 @@
 using LBoL.ConfigData;
 using LBoL.Core;
 using LBoL.Core.Battle;
-using LBoL.Core.Battle.BattleActions;
-using LBoL.Core.Battle.Interactions;
 using LBoL.Core.Cards;
 using LBoLEntitySideloader;
 using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Abilities;
 using LBoLMod.StatusEffects.Keywords;
-using LBoLMod.Utils;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace LBoLMod.Cards
 {
@@ -25,13 +22,10 @@ namespace LBoLMod.Cards
         {
             var cardConfig = base.MakeConfig();
             cardConfig.Rarity = Rarity.Uncommon;
-            cardConfig.Type = CardType.Skill;
+            cardConfig.Type = CardType.Ability;
             cardConfig.Colors = new List<ManaColor>() { ManaColor.Red, ManaColor.White };
-            cardConfig.Value1 = 1;
-            cardConfig.UpgradedValue1 = 2;
-            cardConfig.Cost = new ManaGroup() { Hybrid = 1, HybridColor = 2 };
-            cardConfig.Keywords = Keyword.Retain;
-            cardConfig.UpgradedKeywords = Keyword.Retain;
+            cardConfig.Cost = new ManaGroup() { Any = 2, Hybrid = 1, HybridColor = 2 };
+            cardConfig.UpgradedCost = new ManaGroup() { Hybrid = 1, HybridColor = 2 };
             cardConfig.RelativeEffects = new List<string>() { nameof(Frontline), nameof(Assign) };
             cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Frontline), nameof(Assign) };
             return cardConfig;
@@ -41,34 +35,9 @@ namespace LBoLMod.Cards
     [EntityLogic(typeof(FrontlineLoyaltyTaskLevelDef))]
     public sealed class FrontlineLoyaltyTaskLevel : Card
     {
-        public override Interaction Precondition()
-        {
-            List<Card> list = base.Battle.HandZone.Where((Card c) => c is ModFrontlineCard).ToList();
-            return new SelectHandInteraction(1, Value1, list);
-        }
         protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
         {
-            if (!(precondition is SelectHandInteraction frontlineInteraction))
-                yield break;
-
-            var assignBuffs = HaniwaAssignUtils.CreateAssignOptionCards(base.Battle.Player);
-            if (assignBuffs.Count > 0)
-            {
-                var assignInteraction = new SelectCardInteraction(1, 1, assignBuffs);
-                yield return new InteractionAction(assignInteraction);
-                foreach (ModAssignOptionCard optionCard in assignInteraction.SelectedCards)
-                {
-                    optionCard.StatusEffect.NotifyActivating();
-                    int totalLoyalty = frontlineInteraction.SelectedCards.Cast<ModFrontlineCard>().Sum(c => c.RemainingValue);
-                    optionCard.StatusEffect.Level += totalLoyalty;
-                }
-                yield return PerformAction.Wait(0.3f);
-                foreach (ModFrontlineCard card in frontlineInteraction.SelectedCards)
-                {
-                    card.NotifyActivating();
-                    card.ConsumeLoyalty(card.RemainingValue);
-                }
-            }
+            yield return BuffAction<FrontlineLoyaltyTaskLevelSe>();
         }
     }
 }
