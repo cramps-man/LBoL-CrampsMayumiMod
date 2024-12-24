e55d93d1586de485b1a491bde5d08a59d64544f3
cramps-man <cramps-man@hotmail.com>
2024-12-24 18:18:06 +1100

    Add card LoyaltyProtection

---
+++ Source/Cards/3Uncommon/Yellow/LoyaltyProtection.cs
@@ -0,0 +1,43 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.StatusEffects.Abilities;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class LoyaltyProtectionDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(LoyaltyProtection);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Uncommon;
+            cardConfig.Type = CardType.Skill;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
+            cardConfig.Cost = new ManaGroup() { White = 1, Any = 1 };
+            cardConfig.Value1 = 8;
+            cardConfig.UpgradedValue1 = 16;
+            cardConfig.RelativeEffects = new List<string>() { nameof(LoyaltyProtectionSe) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(LoyaltyProtectionSe) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(LoyaltyProtectionDef))]
+    public sealed class LoyaltyProtection : Card
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return BuffAction<LoyaltyProtectionSe>(Value1);
+        }
+    }
+}
---
+++ Source/StatusEffects/Temporary/LoyaltyProtectionSe.cs
@@ -0,0 +1,53 @@
+﻿using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.StatusEffects;
+using LBoL.Core.Units;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.BattleActions;
+using LBoLMod.GameEvents;
+using System.Collections.Generic;
+
+namespace LBoLMod.StatusEffects.Abilities
+{
+    public sealed class LoyaltyProtectionSeDef : ModStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(LoyaltyProtectionSe);
+        }
+    }
+
+    [EntityLogic(typeof(LoyaltyProtectionSeDef))]
+    public sealed class LoyaltyProtectionSe: StatusEffect
+    {
+        protected override void OnAdded(Unit unit)
+        {
+            base.ReactOwnerEvent(ModGameEvents.ConsumingLoyalty, this.OnConsumingLoyalty);
+        }
+
+        private IEnumerable<BattleAction> OnConsumingLoyalty(ConsumeLoyaltyEventArgs args)
+        {
+            if (args.LoyaltyConsumption <= 0)
+                yield break;
+
+            int amountReduced = 0;
+            if (Level > args.LoyaltyConsumption)
+            {
+                Level -= args.LoyaltyConsumption;
+                amountReduced += args.LoyaltyConsumption;
+                args.LoyaltyConsumption = 0;
+            }
+            else
+            {
+                args.LoyaltyConsumption -= Level;
+                amountReduced += Level;
+                Level = 0;
+            }
+
+            yield return new CastBlockShieldAction(base.Owner, amountReduced, 0);
+            if (Level <= 0)
+                yield return new RemoveStatusEffectAction(this);
+        }
+    }
+}
