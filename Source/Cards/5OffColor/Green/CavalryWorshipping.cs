38b6694411d76037064ce85493caf5e88c77bcbd
cramps-man <cramps-man@hotmail.com>
2025-01-06 19:29:17 +1100

    Add card CavalryWorshipping

---
+++ Source/Cards/5OffColor/Green/CavalryWorshipping.cs
@@ -0,0 +1,49 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Cards;
+using LBoL.Core.StatusEffects;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.BattleActions;
+using LBoLMod.StatusEffects.Abilities;
+using LBoLMod.StatusEffects.Keywords;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class CavalryWorshippingDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(CavalryWorshipping);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Uncommon;
+            cardConfig.Type = CardType.Ability;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.Green };
+            cardConfig.Cost = new ManaGroup() { Green = 1, Any = 2 };
+            cardConfig.UpgradedCost = new ManaGroup() { Green = 1, Any = 1 };
+            cardConfig.Value1 = 1;
+            cardConfig.Value2 = 10;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Haniwa), nameof(Graze) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Haniwa), nameof(Graze) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(CavalryWorshippingDef))]
+    public sealed class CavalryWorshipping : Card
+    {
+        public int CavalryGain => IsUpgraded ? 5 : 3;
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return BuffAction<CavalryWorshippingSe>(Value1);
+            yield return new GainHaniwaAction(cavalryToGain: CavalryGain);
+        }
+    }
+}
---
+++ Source/StatusEffects/Abilities/CavalryWorshippingSe.cs
@@ -0,0 +1,53 @@
+﻿using LBoL.ConfigData;
+using LBoL.Core.Battle;
+using LBoL.Core.StatusEffects;
+using LBoL.Core.Units;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.BattleActions;
+using LBoLMod.GameEvents;
+using System.Collections.Generic;
+
+namespace LBoLMod.StatusEffects.Abilities
+{
+    public sealed class CavalryWorshippingSeDef : ModStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(CavalryWorshippingSe);
+        }
+        public override StatusEffectConfig MakeConfig()
+        {
+            var config = base.MakeConfig();
+            config.HasCount = true;
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(CavalryWorshippingSeDef))]
+    public sealed class CavalryWorshippingSe: StatusEffect
+    {
+        public int Threshold => 10;
+        protected override void OnAdded(Unit unit)
+        {
+            base.ReactOwnerEvent(ModGameEvents.GainedHaniwa, this.OnGainedHaniwa);
+        }
+
+        private IEnumerable<BattleAction> OnGainedHaniwa(GainHaniwaEventArgs args)
+        {
+            if (args.CavalryToGain <= 0)
+                yield break;
+
+            Count += args.CavalryToGain;
+            int timesToActivate = Count / Threshold;
+            if (Count >= Threshold)
+                Count -= Threshold * timesToActivate;
+
+            if (timesToActivate > 0)
+            {
+                base.NotifyActivating();
+                yield return BuffAction<Graze>(timesToActivate * Level);
+            }
+        }
+    }
+}
