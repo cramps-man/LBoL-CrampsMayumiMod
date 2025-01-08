c1257985255ec495dad11a89959d55687157841f
cramps-man <cramps-man@hotmail.com>
2025-01-08 18:42:38 +1100

    Add card FencerWorshipping

---
+++ Source/Cards/5OffColor/Blue/FencerWorshipping.cs
@@ -0,0 +1,48 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.BattleActions;
+using LBoLMod.StatusEffects.Abilities;
+using LBoLMod.StatusEffects.Keywords;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class FencerWorshippingDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(FencerWorshipping);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Uncommon;
+            cardConfig.Type = CardType.Ability;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.Blue };
+            cardConfig.Cost = new ManaGroup() { Blue = 1, Any = 2 };
+            cardConfig.UpgradedCost = new ManaGroup() { Blue = 1, Any = 1 };
+            cardConfig.Value1 = 20;
+            cardConfig.Value2 = 10;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Haniwa) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Haniwa) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(FencerWorshippingDef))]
+    public sealed class FencerWorshipping : Card
+    {
+        public int FencerGain => IsUpgraded ? 5 : 3;
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return BuffAction<FencerWorshippingSe>(Value1);
+            yield return new GainHaniwaAction(fencerToGain: FencerGain);
+        }
+    }
+}
---
+++ Source/StatusEffects/Abilities/FencerWorshippingSe.cs
@@ -0,0 +1,55 @@
+﻿using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.StatusEffects;
+using LBoL.Core.Units;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.BattleActions;
+using LBoLMod.GameEvents;
+using System.Collections.Generic;
+
+namespace LBoLMod.StatusEffects.Abilities
+{
+    public sealed class FencerWorshippingSeDef : ModStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(FencerWorshippingSe);
+        }
+        public override StatusEffectConfig MakeConfig()
+        {
+            var config = base.MakeConfig();
+            config.HasCount = true;
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(FencerWorshippingSeDef))]
+    public sealed class FencerWorshippingSe: StatusEffect
+    {
+        public int Threshold => 10;
+        protected override void OnAdded(Unit unit)
+        {
+            base.ReactOwnerEvent(ModGameEvents.GainedHaniwa, this.OnGainedHaniwa);
+        }
+
+        private IEnumerable<BattleAction> OnGainedHaniwa(GainHaniwaEventArgs args)
+        {
+            if (args.FencerToGain <= 0)
+                yield break;
+
+            Count += args.FencerToGain;
+            int timesToActivate = Count / Threshold;
+            if (Count >= Threshold)
+                Count -= Threshold * timesToActivate;
+
+            if (timesToActivate > 0)
+            {
+                base.NotifyActivating();
+                yield return new DamageAction(Owner, base.Battle.RandomAliveEnemy, DamageInfo.Reaction(timesToActivate * Level));
+            }
+        }
+    }
+}
