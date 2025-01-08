9e348346eaec423c1fac74645f42c338f2054e45
cramps-man <cramps-man@hotmail.com>
2025-01-08 19:23:47 +1100

    Add card ArcherWorshipping

---
+++ Source/Cards/5OffColor/Purple/ArcherWorshipping.cs
@@ -0,0 +1,50 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Cards;
+using LBoL.Core.StatusEffects;
+using LBoL.EntityLib.StatusEffects.Others;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.BattleActions;
+using LBoLMod.StatusEffects.Abilities;
+using LBoLMod.StatusEffects.Keywords;
+using System.Collections.Generic;
+
+namespace LBoLMod.Cards
+{
+    public sealed class ArcherWorshippingDef : ModCardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(ArcherWorshipping);
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = base.MakeConfig();
+            cardConfig.Rarity = Rarity.Uncommon;
+            cardConfig.Type = CardType.Ability;
+            cardConfig.Colors = new List<ManaColor>() { ManaColor.Black };
+            cardConfig.Cost = new ManaGroup() { Black = 1, Any = 2 };
+            cardConfig.UpgradedCost = new ManaGroup() { Black = 1, Any = 1 };
+            cardConfig.Value1 = 1;
+            cardConfig.Value2 = 10;
+            cardConfig.RelativeEffects = new List<string>() { nameof(Haniwa), nameof(Weak), nameof(Vulnerable), nameof(TempFirepowerNegative), nameof(LockedOn), nameof(Poison) };
+            cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Haniwa), nameof(Weak), nameof(Vulnerable), nameof(TempFirepowerNegative), nameof(LockedOn), nameof(Poison) };
+            return cardConfig;
+        }
+    }
+
+    [EntityLogic(typeof(ArcherWorshippingDef))]
+    public sealed class ArcherWorshipping : Card
+    {
+        public int ArcherGain => IsUpgraded ? 5 : 3;
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return BuffAction<ArcherWorshippingSe>(Value1);
+            yield return new GainHaniwaAction(archerToGain: ArcherGain);
+        }
+    }
+}
---
+++ Source/StatusEffects/Abilities/ArcherWorshippingSe.cs
@@ -0,0 +1,65 @@
+﻿using LBoL.ConfigData;
+using LBoL.Core.Battle;
+using LBoL.Core.StatusEffects;
+using LBoL.Core.Units;
+using LBoL.EntityLib.StatusEffects.Others;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLMod.BattleActions;
+using LBoLMod.GameEvents;
+using System.Collections.Generic;
+
+namespace LBoLMod.StatusEffects.Abilities
+{
+    public sealed class ArcherWorshippingSeDef : ModStatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(ArcherWorshippingSe);
+        }
+        public override StatusEffectConfig MakeConfig()
+        {
+            var config = base.MakeConfig();
+            config.HasCount = true;
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(ArcherWorshippingSeDef))]
+    public sealed class ArcherWorshippingSe: StatusEffect
+    {
+        public int Threshold => 10;
+        protected override void OnAdded(Unit unit)
+        {
+            base.ReactOwnerEvent(ModGameEvents.GainedHaniwa, this.OnGainedHaniwa);
+        }
+
+        private IEnumerable<BattleAction> OnGainedHaniwa(GainHaniwaEventArgs args)
+        {
+            if (args.ArcherToGain <= 0)
+                yield break;
+
+            Count += args.ArcherToGain;
+            int timesToActivate = Count / Threshold;
+            if (Count >= Threshold)
+                Count -= Threshold * timesToActivate;
+
+            if (timesToActivate > 0)
+            {
+                base.NotifyActivating();
+                for (int i = 0; i < timesToActivate * Level; i++)
+                {
+                    int rng = base.GameRun.BattleRng.NextInt(1, 5);
+                    switch (rng)
+                    {
+                        case 1: yield return DebuffAction<Weak>(base.Battle.RandomAliveEnemy, duration: 2); break;
+                        case 2: yield return DebuffAction<Vulnerable>(base.Battle.RandomAliveEnemy, duration: 2); break;
+                        case 3: yield return DebuffAction<TempFirepowerNegative>(base.Battle.RandomAliveEnemy, level: 3); break;
+                        case 4: yield return DebuffAction<LockedOn>(base.Battle.RandomAliveEnemy, level: 5); break;
+                        case 5: yield return DebuffAction<Poison>(base.Battle.RandomAliveEnemy, level: 5); break;
+                    }
+                }
+            }
+        }
+    }
+}
