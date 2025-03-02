8a6bb48ef87e5b7f9ba81e96605c6670b6191542
cramps-man <cramps-man@hotmail.com>
2025-03-03 02:19:52 +1100

    FrontlineSpellcard rework to add frontlines to hand

--- Source/UltimateSkills/FrontlineUltimateSkill.cs
+++ Source/UltimateSkills/FrontlineUltimateSkill.cs
@@ -3,13 +3,17 @@ using LBoL.ConfigData;
 using LBoL.Core;
 using LBoL.Core.Battle;
 using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Battle.Interactions;
+using LBoL.Core.Cards;
 using LBoL.Core.Units;
 using LBoLEntitySideloader;
 using LBoLEntitySideloader.Attributes;
 using LBoLEntitySideloader.Entities;
 using LBoLEntitySideloader.Resource;
 using LBoLMod.BattleActions;
+using LBoLMod.Cards;
 using LBoLMod.StatusEffects.Keywords;
+using LBoLMod.Utils;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -41,12 +45,12 @@ namespace LBoLMod.UltimateSkills
                 PowerPerLevel: 100,
                 MaxPowerLevel: 2,
                 RepeatableType: UsRepeatableType.OncePerTurn,
-                Damage: 40,
+                Damage: 0,
                 Value1: 5,
-                Value2: 0,
-                Keywords: Keyword.Accuracy,
-                RelativeEffects: new List<string>() { nameof(Haniwa) },
-                RelativeCards: new List<string>() { }
+                Value2: 3,
+                Keywords: Keyword.None,
+                RelativeEffects: new List<string>() { nameof(Haniwa), nameof(Frontline) },
+                RelativeCards: new List<string>() { nameof(HaniwaAttacker), nameof(HaniwaBodyguard), nameof(HaniwaUpgrader), nameof(HaniwaExploiter) }
                 );
 
             return config;
@@ -57,14 +61,34 @@ namespace LBoLMod.UltimateSkills
     [EntityLogic(typeof(FrontlineUltimateSkillDef))]
     public sealed class FrontlineUltimateSkill : UltimateSkill
     {
+        public string InteractionTitle => this.LocalizeProperty("InteractionTitle").RuntimeFormat(this.FormatWrapper);
         public FrontlineUltimateSkill() {
-            base.TargetType = TargetType.SingleEnemy;
+            base.TargetType = TargetType.Self;
         }
         protected override IEnumerable<BattleAction> Actions(UnitSelector selector)
         {
-            var unit = base.Battle.Player;
             yield return new GainHaniwaAction(Value1, Value1, Value1);
-            yield return new DamageAction(base.Owner, selector.GetEnemy(base.Battle), Damage);
+            yield return PerformAction.Wait(0.2f);
+            List<ModFrontlineOptionCard> cards = HaniwaFrontlineUtils.GetAllOptionCards(base.Battle, checkSacrificeRequirement: true);
+            var selectCardInteraction = new SelectCardInteraction(0, Value2, cards);
+            selectCardInteraction.Description = InteractionTitle;
+            yield return new InteractionAction(selectCardInteraction);
+            foreach (var action in HaniwaFrontlineUtils.CardsSummon(selectCardInteraction.SelectedCards))
+            {
+                if (action is AddCardsToHandAction addHand)
+                {
+                    List<Card> beforeList = new List<Card>(addHand.Args.Cards);
+                    yield return addHand;
+                    List<Card> afterList = new List<Card>(addHand.Args.Cards); //only contains cards added to hand
+                    if (beforeList.Count != afterList.Count)
+                    {
+                        beforeList.RemoveAll(c => afterList.Contains(c));
+                        yield return new AddCardsToDrawZoneAction(beforeList, DrawZoneTarget.Random);
+                    }
+                }
+                else
+                    yield return action;
+            };
         }
     }
 }
