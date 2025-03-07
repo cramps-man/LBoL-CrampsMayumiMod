a41c77517bc37172c4b424d02d2ea8505739fbc1
cramps-man <cramps-man@hotmail.com>
2025-03-07 17:48:24 +1100

    LoyaltyProtection card allow executing 1 frontline card onplay
    - And rename to LoyaltyCommand

--- Source/Cards/3Uncommon/Yellow/LoyaltyCommand.cs
+++ Source/Cards/3Uncommon/Yellow/LoyaltyCommand.cs
@@ -2,19 +2,22 @@
 using LBoL.ConfigData;
 using LBoL.Core;
 using LBoL.Core.Battle;
+using LBoL.Core.Battle.Interactions;
 using LBoL.Core.Cards;
 using LBoLEntitySideloader;
 using LBoLEntitySideloader.Attributes;
 using LBoLMod.StatusEffects.Abilities;
+using LBoLMod.Utils;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LBoLMod.Cards
 {
-    public sealed class LoyaltyProtectionDef : ModCardTemplate
+    public sealed class LoyaltyCommandDef : ModCardTemplate
     {
         public override IdContainer GetId()
         {
-            return nameof(LoyaltyProtection);
+            return nameof(LoyaltyCommand);
         }
 
         public override CardConfig MakeConfig()
@@ -22,26 +25,37 @@ namespace LBoLMod.Cards
             var cardConfig = base.MakeConfig();
             cardConfig.Rarity = Rarity.Uncommon;
             cardConfig.Type = CardType.Skill;
+            cardConfig.TargetType = TargetType.SingleEnemy;
             cardConfig.Colors = new List<ManaColor>() { ManaColor.White };
             cardConfig.Cost = new ManaGroup() { White = 1 };
             cardConfig.UpgradedCost = new ManaGroup() { Any = 1 };
-            cardConfig.Block = 4;
-            cardConfig.UpgradedBlock = 6;
             cardConfig.Value1 = 6;
             cardConfig.UpgradedValue1 = 10;
+            cardConfig.Value2 = 1;
             cardConfig.RelativeEffects = new List<string>() { nameof(LoyaltyProtectionSe) };
             cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(LoyaltyProtectionSe) };
             return cardConfig;
         }
     }
 
-    [EntityLogic(typeof(LoyaltyProtectionDef))]
-    public sealed class LoyaltyProtection : Card
+    [EntityLogic(typeof(LoyaltyCommandDef))]
+    public sealed class LoyaltyCommand : Card
     {
+        public override Interaction Precondition()
+        {
+            List<Card> list = base.Battle.HandZone.Where((Card c) => c != this && c is ModFrontlineCard).ToList();
+            return new SelectHandInteraction(0, Value2, list);
+        }
         protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
         {
-            yield return DefenseAction();
             yield return BuffAction<LoyaltyProtectionSe>(Value1);
+            if (!(precondition is SelectHandInteraction selectInteraction))
+                yield break;
+
+            foreach (var battleAction in HaniwaFrontlineUtils.ExecuteOnPlayActions(selectInteraction.SelectedCards.ToList(), base.Battle, selector, true))
+            {
+                yield return battleAction;
+            }
         }
     }
 }
