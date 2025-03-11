bd5f900e1982bcf8dbfd2f50bd7649bdc2b3bdda
cramps-man <cramps-man@hotmail.com>
2025-03-12 00:15:45 +1100

    MassTaskLevel adjust mana cost

--- Source/Cards/4Rare/Red/MassTaskLevel.cs
+++ Source/Cards/4Rare/Red/MassTaskLevel.cs
@@ -31,7 +31,7 @@ namespace LBoLMod.Cards
             cardConfig.Colors = new List<ManaColor>() { ManaColor.Red };
             cardConfig.Value1 = 3;
             cardConfig.Value2 = 30;
-            cardConfig.Cost = new ManaGroup() { Red = 3 };
+            cardConfig.Cost = new ManaGroup() { Red = 2, Any = 1 };
             cardConfig.UpgradedCost = new ManaGroup() { Red = 1 };
             cardConfig.RelativeEffects = new List<string>() { nameof(Assign) };
             cardConfig.UpgradedRelativeEffects = new List<string>() { nameof(Assign) };
