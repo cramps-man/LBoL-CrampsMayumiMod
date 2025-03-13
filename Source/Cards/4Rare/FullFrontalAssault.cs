8b01cd8b6ca3aed3dabb121066a220fc5d05f131
cramps-man <cramps-man@hotmail.com>
2025-03-13 20:06:35 +1100

    FullFrontalAssault nerf bonus dmg a bit
    - Long time coming, values were probs a bit too high tbh, dmg 73(100) on battle start haniwas

--- Source/Cards/4Rare/FullFrontalAssault.cs
+++ Source/Cards/4Rare/FullFrontalAssault.cs
@@ -31,8 +31,8 @@ namespace LBoLMod.Cards
             cardConfig.Cost = new ManaGroup() { White = 1, Red = 1, Any = 1 };
             cardConfig.UpgradedCost = new ManaGroup() { Any = 2, Hybrid = 1, HybridColor = 2 };
             cardConfig.Damage = 10;
-            cardConfig.Value1 = 7;
-            cardConfig.UpgradedValue1 = 10;
+            cardConfig.Value1 = 6;
+            cardConfig.UpgradedValue1 = 8;
             cardConfig.Value2 = 3;
             cardConfig.Keywords = Keyword.Exile | Keyword.Retain;
             cardConfig.UpgradedKeywords = Keyword.Exile | Keyword.Retain;
