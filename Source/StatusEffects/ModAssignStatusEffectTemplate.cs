0e59e0ccbe6d967f78bab5fc61c2cc74194ac5db
cramps-man <cramps-man@hotmail.com>
2025-03-22 20:33:46 +1100

    Art assignment arrow barrage

--- Source/StatusEffects/ModAssignStatusEffectTemplate.cs
+++ Source/StatusEffects/ModAssignStatusEffectTemplate.cs
@@ -1,17 +1,15 @@
 ﻿using LBoL.Base;
 using LBoL.ConfigData;
-using LBoLEntitySideloader.Resource;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace LBoLMod.StatusEffects
 {
     public abstract class ModAssignStatusEffectTemplate: ModStatusEffectTemplate
     {
-        public override Sprite LoadSprite()
+        /*public override Sprite LoadSprite()
         {
             return ResourceLoader.LoadSprite("assignicon.png", BepinexPlugin.embeddedSource);
-        }
+        }*/
         public override StatusEffectConfig MakeConfig()
         {
             return new StatusEffectConfig(
